2be974604a198f91b9dfb36135e8fc24b93af57e
joshroxas-dev <joshuabroxas@gmail.com>
2021-09-09 20:29:45 +0800

    added google sign in

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,62 +1,153 @@
-import React, {useState} from 'react'
-import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core'
+import React, { useState } from "react";
+import {
+  Avatar,
+  Button,
+  Paper,
+  Grid,
+  Typography,
+  Container,
+} from "@material-ui/core";
+
+import { useHistory } from "react-router-dom";
+import GoogleLogin from "react-google-login";
+
+import { useDispatch } from "react-redux";
 
 import Input from "./Input";
 
 import useStyles from "./styles";
 
 import LockedOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Icon from "./Icon";
 
 const Auth = () => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-    const classes = useStyles();
+  const [isSignUp, setIsSignUp] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
-    const isSignUp = true;
+  const switchMode = () => {
+    setIsSignUp(!isSignUp);
+    setShowPassword(false);
+  };
 
-    const [showPassword, setShowPassword] = useState(false);
+  const handleSubmit = () => {};
 
-    const handleSubmit = () => {
+  const handleChange = () => {};
 
+  const handleShowPassword = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
+  const googleSuccess = async (res) => {
+    const result = res?.profileObj;
+    const token = res?.tokenId;
+
+    try {
+      dispatch({ type: "AUTH", data: { result, token } });
+      history.push("/");
+    } catch (error) {
+      console.log("google sign in", error);
     }
+  };
 
-    const handleChange = () => {
+  const googleFailure = (error) => {
+    console.log("Google sign in was unsuccessful try again", error);
+  };
 
-    } 
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockedOutlinedIcon />
+        </Avatar>
+        <Typography variant="h5">{isSignUp ? "Sign Up" : "Sign In"}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {isSignUp && (
+              <>
+                <Input
+                  name="firstName"
+                  label="First Name"
+                  handleChange={handleChange}
+                  autoFocus
+                  half
+                />
+                <Input
+                  name="lastName"
+                  label="Last Name"
+                  handleChange={handleChange}
+                  half
+                />
+              </>
+            )}
+            <Input
+              name="email"
+              label="Email Address"
+              handleChange={handleChange}
+              type="email"
+            />
+            <Input
+              name="password"
+              label="Password"
+              handleChange={handleChange}
+              type={showPassword ? "text" : "password"}
+              handleShowPassword={handleShowPassword}
+            />
+            {isSignUp && (
+              <Input
+                name="confirmPassword"
+                label="Confirm Password"
+                handleChange={handleChange}
+                type={showPassword ? "text" : "password"}
+                handleShowPassword={handleShowPassword}
+              />
+            )}
+          </Grid>
 
-    const handleShowPassword = () => {
-        setShowPassword((prevShowPassword) => !prevShowPassword);
-    }
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Submit
+          </Button>
+          <GoogleLogin
+            clientId="453567097823-gtjhgl7aotlm98o8soijb296dj3bpnll.apps.googleusercontent.com"
+            render={(renderProps) => (
+              <Button
+                className={classes.googleButton}
+                color="primary"
+                fullWidth
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                startIcon={<Icon />}
+                variant="contained"
+              >
+                Google Sign in
+              </Button>
+            )}
+            onSuccess={googleSuccess}
+            onFailure={googleFailure}
+            cookiePolicy="single_host_origin"
+          />
+          <Button container justify="flex-end">
+            <Grid item>
+              <Button onClick={switchMode}>
+                {isSignUp
+                  ? "Already have and account? Sign in."
+                  : "Don't have an account? Sign up."}
+              </Button>
+            </Grid>
+          </Button>
+        </form>
+      </Paper>
+    </Container>
+  );
+};
 
-    return (
-        <Container component="main" maxWidth="xs">
-            <Paper className={classes.paper} elevation={3}>
-                <Avatar className={classes.avatar}>
-                    <LockedOutlinedIcon />
-                </Avatar>
-                <Typography variant="h5">{isSignUp ? "Sign Up" : "Sign In"}</Typography>
-                <form className={classes.form} onSubmit={handleSubmit}>
-                    <Grid container spacing={2}>
-                        {
-                            isSignUp && (
-                                <>
-                                    <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
-                                    <Input name="lastName" label="Last Name" handleChange={handleChange} half />
-                                </>
-                            )
-                        }
-                        <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
-                        <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
-                        {
-                            isSignUp && (
-                                <Input name="confirmPassword" label="Confirm Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
-                            )
-                        }
-                    </Grid>
-                    <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>Submit</Button>
-                </form>
-            </Paper>
-        </Container>
-    )
-}
-
-export default Auth
+export default Auth;
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,62 +1,87 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import { AppBar, Avatar, Toolbar, Typography, Button } from "@material-ui/core";
-import { Link } from "react-router-dom";
-
+import { Link, useHistory, useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import memories from "../../images/memories.png";
 
 import useStyles from "./styles";
 
-
 const Navbar = () => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const location = useLocation();
 
-	const classes = useStyles();
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
 
-	const user = null;
+  const logout = () => {
+    dispatch({ type: "LOGOUT" });
+    setUser(null);
+    history.push("/");
+  };
 
-	return (
-		<AppBar
-			className={classes.appBar}
-			position="static"
-			color="inherit"
-		>
-			<div className={classes.brandContainer}>
-				<Typography
-					component={Link}
-					to="/"
-					className={classes.heading}
-					variant="h2"
-					align="center"
-				>
-					Memories
-				</Typography>
-				<img
-					className={classes.image}
-					src={memories}
-					alt="memories"
-					height="60"
-				/>
-			</div>
-			<Toolbar className={classes.toolbar}>
-				{
-					user ?(
-						<div className={classes.profile}>
-							<Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
-							<Typography className={classes.userName} variant="h6">
-								{user.result.name}
-							</Typography>
-							<Button className={classes.logout} variant="contained" color="secondary">
-								Log out
-							</Button>
-						</div>
-					) :(
-						<div>
-							<Button component={Link} to="/auth" variant="contained" color="primary">Sign in</Button>
-						</div>
-					)
-				}
-			</Toolbar>
-		</AppBar>
-	)
-}
+  useEffect(() => {
+    const token = user?.token;
 
-export default Navbar
+    setUser(JSON.parse(localStorage.getItem("profile")));
+  }, [location]);
+
+  return (
+    <AppBar className={classes.appBar} position="static" color="inherit">
+      <div className={classes.brandContainer}>
+        <Typography
+          component={Link}
+          to="/"
+          className={classes.heading}
+          variant="h2"
+          align="center"
+        >
+          Memories
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
+      </div>
+      <Toolbar className={classes.toolbar}>
+        {user ? (
+          <div className={classes.profile}>
+            <Avatar
+              className={classes.purple}
+              alt={user.result.name}
+              src={user.result.imageUrl}
+            >
+              {user.result.name.charAt(0)}
+            </Avatar>
+            <Typography className={classes.userName} variant="h6">
+              {user.result.name}
+            </Typography>
+            <Button
+              className={classes.logout}
+              variant="contained"
+              color="secondary"
+              onClick={logout}
+            >
+              Log out
+            </Button>
+          </div>
+        ) : (
+          <div>
+            <Button
+              component={Link}
+              to="/auth"
+              variant="contained"
+              color="primary"
+            >
+              Sign in
+            </Button>
+          </div>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
